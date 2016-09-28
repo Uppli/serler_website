05ea6d6c7704b0677867015badb41fdc22819c63
Uppli <uppli.esu@gmail.com>
2016-09-28 13:14:01 +0000

    Integrating travis ci

---
+++ spec/features/articles_tests_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+
+describe "Serler" do
+end
+RSpec.describe Article, :type => :model do
+  it "is valid with valid attributes" do
+    expect(Article.new).to_not be_valid
+  end
+
+  it "is not valid without a title" do
+  article = Article.new(title: nil)
+  expect(article).to_not be_valid
+  end
+end
+
+describe "SERLER" do
+  it "should have the  'SERLER'" do
+  visit '/'
+  page.should have_selector('h1', :text => 'SERLER')
+  end
+  it "should have the 'Listing articles'" do
+  visit '/articles'
+  page.should have_selector('h1', :text => 'Listing articles')
+  end
+  it "should have the 'Advanced Search'" do
+  visit '/searches/new'
+  page.should have_selector('h1', :text => 'Advanced Search')
+  end
+end
+
