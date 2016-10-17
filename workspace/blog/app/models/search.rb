56e269d36872ec0942a7dfb3c6775bae7cf02ed1
Huali ZHOU <hualizhouster@gmail.com>
2016-10-17 03:03:45 +0000

    search_hydra

--- workspace/blog/app/models/search.rb
+++ workspace/blog/app/models/search.rb
@@ -3,30 +3,31 @@ class Search < ApplicationRecord
 def search_articles
         articles = Article.all
 
-      
-    $where = " "
+    
+   # $where = " "
 
-    for i in 0.."#{keywords1}".split('$').length-1
+   # for i in 0.."#{keywords1}".split('$').length-1
         
         
-        if "#{keywords1}".split('$')[i].split('#')[0]=="-1" and  "#{keywords1}".split('$')[i].split('#')[2] =="-1"
-            $where =$where+ " 1=1 "
-         end
+     #   if "#{keywords1}".split('$')[i].split('#')[0]=="-1" and  "#{keywords1}".split('$')[i].split('#')[2] =="-1"
+     #       $where =$where+ " 1=1 "
+     #    end
         
-         if "#{keywords1}".split('$')[i].split('#')[0]=="-1" and  "#{keywords1}".split('$')[i].split('#')[2] !="-1"
-                $where=$where+"  "+ "#{keywords1}".split('$')[i].split('#')[1]+" like  \'%" + "#{keywords1}".split('$')[i].split('#')[2]+'%\''
-         end
+      #   if "#{keywords1}".split('$')[i].split('#')[0]=="-1" and  "#{keywords1}".split('$')[i].split('#')[2] !="-1"
+      #          $where=$where+"  "+ "#{keywords1}".split('$')[i].split('#')[1]+" like  \'%" + "#{keywords1}".split('$')[i].split('#')[2]+'%\''
+     #    end
            
-         if "#{keywords1}".split('$')[i].split('#')[0] !="-1" and  "#{keywords1}".split('$')[i].split('#')[2] =="-1"
-               $where =$where+ " 1=1 "
-         end
-        
-         if "#{keywords1}".split('$')[i].split('#')[0] !="-1" and  "#{keywords1}".split('$')[i].split('#')[2] !="-1" 
-               $where=$where +"  "+ "#{keywords1}".split('$')[i].split('#')[0]+" " + "#{keywords1}".split('$')[i].split('#')[1]+" like  \'%" + "#{keywords1}".split('$')[i].split('#')[2]+'%\''
-         end
+      #   if "#{keywords1}".split('$')[i].split('#')[0] !="-1" and  "#{keywords1}".split('$')[i].split('#')[2] =="-1"
+     #          $where =$where+ " 1=1 "
+     #    end
+      #  
+     #    if "#{keywords1}".split('$')[i].split('#')[0] !="-1" and  "#{keywords1}".split('$')[i].split('#')[2] !="-1" 
+     #          $where=$where +" fff "+ "#{keywords1}".split('$')[i].split('#')[0]+" " + "#{keywords1}".split('$')[i]+" like  \'%" + "#{keywords1}".split('$')[i].split('#')[2]+'%\''
+     #    end
 
           
-    end
+   # end
+
 
     articles = articles.where($where)
        
@@ -34,4 +35,5 @@ def search_articles
 
 end
 end
+
 
