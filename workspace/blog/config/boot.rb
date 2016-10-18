ec3af7390dea4efbae559612449e904d3fc91438
Huali ZHOU <hualizhouster@gmail.com>
2016-10-18 00:54:25 +0000

    updates input function, favourite function, pdf export

--- workspace/blog/config/boot.rb
+++ workspace/blog/config/boot.rb
@@ -1,3 +1,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+ENV['NLS_LANG'] = 'AMERICAN_AMERICA.UTF8'
+ENV['NLS_LANG'] = 'AMERICAN_AMERICA.ASCII-8BIT'
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
